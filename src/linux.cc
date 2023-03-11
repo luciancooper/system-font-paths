f1e4cf570d14ea82a7a6c2ed96e8bf0749b96859
Lucian Cooper <cooperlucian@gmail.com>
2023-03-10 19:41:29 -0500

    fix: rewrite linux implementation

--- src/linux.cc
+++ src/linux.cc
@@ -1,28 +1,32 @@
 #include <fontconfig/fontconfig.h>
-
 #include <string>
-
 #include "collection.h"
 
 StringVector *getSystemFonts() {
     StringVector *res = new StringVector();
-    FcConfig *config = FcInitLoadConfigAndFonts();
-    if (!config) {
-        return res;
-    }
-    FcFontSet *fontSet = FcConfigGetFonts(config, FcSetSystem);
-    if (!fontSet) {
-        FcConfigDestroy(config);
-        return res;
-    }
-    for (int i = 0; i < fontSet->nfont; i++) {
-        FcChar8 *path;
-        FcPattern *pattern = fontSet->fonts[i];
-        if (FcPatternGetString(pattern, FC_FILE, 0, &path) == FcResultMatch) {
-            res->push_back(std::string(reinterpret_cast<const char *>(path)));
+    // initialize the fontconfig library
+    FcInit();
+    // create an empty fontconfig pattern
+    FcPattern *pattern = FcPatternCreate();
+    // specify the font properties to retrieve
+    FcObjectSet *objectSet = FcObjectSetBuild(FC_FILE, nullptr);
+    // retrieve the font files
+    FcFontSet *fontSet = FcFontList(nullptr, pattern, objectSet);
+    if (fontSet) {
+        for (int i = 0; i < fontSet->nfont; i++) {
+            FcChar8 *fontFile;
+            if (FcPatternGetString(fontSet->fonts[i], FC_FILE, 0, &fontFile) == FcResultMatch) {
+                // convert the font file path from FcChar8 to std:string and add it to result set
+                res->push_back(std::string(reinterpret_cast<char *>(fontFile)));
+            }
         }
+        // cleanup the font set
+        FcFontSetDestroy(fontSet);
     }
-    FcFontSetDestroy(fontSet);
-    FcConfigDestroy(config);
+    // cleanup
+    FcObjectSetDestroy(objectSet);
+    FcPatternDestroy(pattern);
+    // finalize the fontconfig library
+    FcFini();
     return res;
 }
